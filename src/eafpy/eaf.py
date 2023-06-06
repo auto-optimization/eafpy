45c9a3290ee2d652da12944d2993665bcd8f0103
fergus rooney <fergus592000@gmail.com>
2023-06-06 15:46:36 +0100

    Documented research about which C wrapping tool to use. Setup for github actions automatic testing

---
+++ src/eafpy/eaf.py
@@ -0,0 +1,3 @@
+''' Example function for setting up tests'''
+def add_one(number):
+    return number + 1
---
+++ tests/t_eaf.py
@@ -0,0 +1,17 @@
+import sys
+import os
+import pytest
+
+# Add the path of the package top level so that the src.eafpy import works
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+
+from src.eafpy import eaf
+
+
+def test_add_one_five():
+    assert eaf.add_one(5) == 6
+
+def test_add_one_six():
+    assert eaf.add_one(6) == 7
