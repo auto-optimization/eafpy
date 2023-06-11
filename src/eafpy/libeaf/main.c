ca0a341bda00f05696f712ebe3abde673fa70d3f
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-06-11 21:53:25 +0100

    Update main.c

--- src/eafpy/libeaf/main.c
+++ src/eafpy/libeaf/main.c
@@ -1,15 +1,22 @@
+/* In Linux, I can compile this with:
+   
+   gcc -o main main.c io.c
+*/
 #include "io.h"
 
-int main(){
+int main()
+{
     const char * filename ="input1.dat";
-    double * return_data = 0;
-    int num_obj=10;
-    int * cumsizes;
-    int numsets=10;
-    int x = read_double_data(filename, &return_data,&num_obj, &cumsizes, &numsets);
-    printf("%d", &x);
-
-
-
+    double * return_data = NULL;
+    int num_obj = 0;
+    int *cumsizes = NULL;
+    int num_sets=0;
+    int x = read_double_data(filename, &return_data, &num_obj, &cumsizes, &num_sets);
+    printf("return value: %d\nnum_obj: %d\nnum_sets: %d\n", x, num_obj, num_sets);
+    printf("cumsizes: {");
+    for (int i = 0; i < num_sets; i++) {
+        printf("%d ", cumsizes[i]);
+    }
+    printf("}\n");
     return 0;
 }
