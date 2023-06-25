124aeb6c8f9932327634d5d36ed2befc622add5e
fergus rooney <fergus592000@gmail.com>
2023-06-25 14:44:00 +0100

    Added support for smoothed 3d surface graphs - issue #5, as well as point-surface graphs. Added some more formatting to graph types. Updated doc with examples [build doc]

--- src/eafpy/plot.py
+++ src/eafpy/plot.py
@@ -3,27 +3,81 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 
+_3d_margin = dict(r=5, l=5, b=20, t=20)
 
-def _parse_line_type(line_type):
+
+def _get_3d_title(title):
+    return {"text": title, "y": 0.9, "x": 0.45, "xanchor": "center", "yanchor": "top"}
+
+
+def _parse_plot_type(line_type, dimension):
     if not isinstance(line_type, str):
         raise TypeError("Type argument must be a string")
     type_arr = line_type.replace(" ", "").lower().split(",")
     if len(type_arr) > 2:
         raise ValueError("Error parsing Type command: Too many commas")
-    has_line = any("lines".startswith(x) for x in type_arr)
-    has_point = any("points".startswith(x) for x in type_arr)
-    if has_line and has_point:
+    allowed_types = ["lines", "points", "surface", "cube"]
+    selected_types = [
+        t for t in allowed_types if any(t.startswith(x) for x in type_arr)
+    ]
+
+    if ("surface" in selected_types or "cube" in selected_types) and dimension == 2:
+        raise ValueError("Surface and cube types only valid for 3 objective plot")
+
+    if "points" in selected_types and "lines" in selected_types:
         return "lines+markers"
-    elif has_point:
+    elif "points" in selected_types and "surface" in selected_types:
+        return "surface+markers"
+    elif "points" in selected_types:
         return "markers"
-    elif has_line:
+    elif "lines" in selected_types:
         return "lines"
+    elif "surface" in selected_types:
+        return "surface"
+    elif "cube" in selected_types:
+        return "cube"
     else:
         raise ValueError(f"Type argument for plot_datasets not recognised: {type}")
 
 
+def _gen_3d_mesh_plot(dataset, type):
+    num_sets = dataset["Set Number"].unique()
+    fig = go.Figure()
+    for set in num_sets:
+        df_one_set = dataset[dataset["Set Number"] == set]
+        fig.add_trace(
+            go.Mesh3d(
+                x=df_one_set["Objective 1"],
+                y=df_one_set["Objective 2"],
+                z=df_one_set["Objective 3"],
+                opacity=0.85,
+                name="Set " + set,
+            ),
+        )
+        if "markers" in type:
+            fig.add_scatter3d(
+                mode="markers",
+                x=df_one_set["Objective 1"],
+                y=df_one_set["Objective 2"],
+                z=df_one_set["Objective 3"],
+                name="Set " + set + " points",
+                marker=dict(size=3),
+            )
+    fig.update_traces(showlegend=True)
+    fig.update_layout(
+        margin=_3d_margin,
+        legend_title_text="Set Number",
+        scene=dict(
+            xaxis_title="Objective 1",
+            yaxis_title="Objective 2",
+            zaxis_title="Objective 3",
+        ),
+        title=_get_3d_title("Surface plot of three objective dataset"),
+    )
+    return fig
+
+
 def plot_datasets(datasets, type="points"):
-    type_parsed = _parse_line_type(type)
     datasets = np.asarray(datasets)
     if not isinstance(datasets, np.ndarray):
         raise TypeError("Dataset must be of type numpy array")
@@ -33,6 +87,7 @@ def plot_datasets(datasets, type="points"):
             "Dataset must contain 2 or 3 objectives "
             "the last column must be the set number"
         )
+    type_parsed = _parse_plot_type(type, dim)
 
     column_names = ["Objective 1", "Objective 2", "Set Number"]
     if dim == 3:
@@ -51,27 +106,27 @@ def plot_datasets(datasets, type="points"):
             x="Objective 1",
             y="Objective 2",
             color="Set Number",
-            title="Two objective dataset",
+            title="Plot of a two objective dataset",
         )
-
         figure.update_traces(line_shape="hv", mode=type_parsed)
 
     elif dim == 3:
-        if "lines" in type_parsed:
-            # I think I will have to add some more complicated calculation here
-            # In order to get a kind of "box" plot
-            figure = go.Figure(data=[go.Surface(z=sets_df.values)])
-
-        else:
+        if "surface" in type_parsed:
+            # Currently creates a surface with points joined for each
+            figure = _gen_3d_mesh_plot(sets_df, type_parsed)
+        elif "markers" in type_parsed:
             figure = px.scatter_3d(
                 sets_df,
                 x="Objective 1",
                 y="Objective 2",
                 z="Objective 3",
                 color="Set Number",
-                title="Three objective dataset",
             )
             figure.update_traces(marker_size=4)
+            figure.update_layout(
+                margin=_3d_margin,
+                title=_get_3d_title("Point plot of three objective dataset"),
+            )
     else:
         raise NotImplementedError
 
