8def54cc65cc0dac135e0b3381db9613272e3790
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-08-04 00:13:29 +0100

    remove redundant information in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    packages=find_packages(),
-    setup_requires=["cffi>=1.0.0"],
-    cffi_modules=["src/eafpy/build_c_eaf.py:ffibuilder"],
-    install_requires=["cffi>=1.0.0"],
-    package_data={"": ["*.h", "*.c"]},
-)
+setup(cffi_modules=["src/eafpy/build_c_eaf.py:ffibuilder"])
