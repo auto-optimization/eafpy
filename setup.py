62f26adf181d40b355a5ac0079b4585aac850c15
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-08-04 00:43:55 +0100

    Undo previous changes

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(cffi_modules=["src/eafpy/build_c_eaf.py:ffibuilder"])
+setup(
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["src/eafpy/build_c_eaf.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
+    package_data={"": ["*.h", "*.c"]},
+)
