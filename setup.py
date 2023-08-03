6f91c7d74ba2e6c1ecb693ebf9e57d00f596430a
MLopez-Ibanez <2620021+MLopez-Ibanez@users.noreply.github.com>
2023-08-04 00:56:00 +0100

    setup.py: Remove install_requires.

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup
 setup(
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["src/eafpy/build_c_eaf.py:ffibuilder"],
-    install_requires=["cffi>=1.0.0"],
     package_data={"": ["*.h", "*.c"]},
 )
